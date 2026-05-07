e72f9b8d36bfd886cc290f40d990ecfdbf9f872c
Tomasz Osuch <tomasz.osuch@gmail.com>
2026-05-07 02:10:57 +0200

    add(): dodanie i przekazanie akcji na CardForm

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,12 +1,20 @@
 import styles from './Column.module.scss';
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
+const Column = ({ id, title, icon, cards, addCard }) => {
 
-const Column = ({ title, icon }) => {
   return (
     <article className={styles.column}>
       <h2 className={styles.title}>
         <span className={`${styles.icon} fa fa-${icon}`}></span>
         {title}
       </h2>
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card key={card.id} title={card.title} />
+        ))}
+      </ul>
+      <CardForm action={(newCard) => addCard(newCard, id)} />
     </article>
   );
 };
